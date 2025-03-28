913e7ac3a8af23eff6e93d5cd8e9a97f9a01627c
shing3tsuu <154595182+shing3tsuu@users.noreply.github.com>
2025-03-28 10:38:44 +0400

    Update botconfig.py

--- botconfig.py
+++ botconfig.py
@@ -7,11 +7,32 @@ class TgBot:
     admin_ids: list[int]
 
 @dataclass
+class DBConfig:
+    host: str
+    port: int
+    name: str
+    user: str
+    password: str
+
+@dataclass
 class Config:
     tg_bot: TgBot
+    db: DBConfig
 
 def load_config(path: str | None) -> Config:
     env = Env()
     env.read_env(path)
 
-    return Config(tg_bot=TgBot(token=env('BOT_TOKEN'), admin_ids=env.list('ADMIN_IDS', subcast=int)))
+    return Config(
+    tg_bot=TgBot(
+        token=env('BOT_TOKEN'),
+        admin_ids=list(map(int, env.list('ADMIN_IDS')))
+    ),
+    db=DBConfig(
+        host=env('DB_HOST'),
+        port=env('DB_PORT'),
+        name=env('DB_NAME'),
+        user=env('DB_USER'),
+        password=env('DB_PASSWORD')
+    )
+)
